9ffa285f741b3483aaaf2000fe132f608805070d
Ángel Orlando VM <A01659000@tec.mx>
2025-03-31 19:54:56 -0600

    avance de front + cubo 3d

--- cubos-frontend/src/app/components/Cube3D.tsx
+++ cubos-frontend/src/app/components/Cube3D.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Canvas } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei"
+import { OrbitControls, useGLTF } from "@react-three/drei"
 
 function CubeMesh(props) {
     const { nodes } = useGLTF("/rubik_cube/rubik_v2.glb")
@@ -24,9 +24,10 @@ export default function Cube3D() {
     return (
         <div id='canvas-container'>
             <Canvas camera={{ position: [-1, 3, -10], fov: 30, near: 1, far: 150 }}>
-                <CubeMesh rotation={[0, 0, Math.PI / 2.0]} position={[10, 0, 5]} />
+                <CubeMesh rotation={[0, 0, Math.PI / 2.0]} position={[0, 0, 0]} />
                 <ambientLight intensity={1.5} />
                 <directionalLight position={[0, 0, 5]} color="white" />
+                <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI * 2 - 0.001}/>
             </Canvas>
         </div>
     )
--- cubos-frontend/src/app/timer/page.tsx
+++ cubos-frontend/src/app/timer/page.tsx
@@ -7,9 +7,9 @@ const onest_300 = Onest({weight: '300'})
 
 export default function Timer() {
     return (
-    <>
-        <aside className={"flex flex-col w-[30%] h-[100vh]"}>
-            <div className={`h-auto py-2  bg-[#36454F] flex justify-center items-center text-[4rem] ${onest_600.className} rounded-br-[1.5rem]`}>
+    <div className={"flex flex-col"}>
+        <aside id="left-sidebar" className={"flex flex-col w-[30%] h-[100vh] float-left fixed"}>
+            <div className={`h-auto py-2  bg-[#36454F] flex justify-center items-center text-[4rem] ${onest_600.className}`}>
                 <h3 className={`${onest_600.className}`}>XD_perm</h3>
             </div>
             <div className={"left-0 top-0 w-[100%] py-4 bg-[#2D3840] flex flex-row flex-wrap place-content-evenly gap-7 relative rounded-br-[1.5rem] px-5"}>
@@ -28,6 +28,11 @@ export default function Timer() {
             </div>
             <Sidebar></Sidebar>
         </aside>
-        <Cube3D/>
-    </>)
+        <div className={"ml-[30%]"}>
+            <div className={"bg-[#495D6A] px-5 py-[2.8rem]"}>
+                <h2 className={"text-center"}>R2 F D2 U2 B L2 B' L2 B U2 B2 R2 L U R' B' D' R2 F U2 R</h2>
+            </div>
+            <Cube3D/>
+        </div>
+    </div>)
 }
